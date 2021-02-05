e4bc006de51a68547a432ec69caeadd393c3f38c
hilalbastan <hilalbastan@gmail.com>
2021-02-06 02:51:57 +0300

    Araç Kiralama Sistemi

---
+++ DataAccess/Concrete/Ef/EfColorDal.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.Ef
+{
+    public class EfColorDal : IColorDal
+    {
+        public void Add(Color entity)
+        {
+            using (CarsContext context = new CarsContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Color entity)
+        {
+            using (CarsContext context = new CarsContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Color Get(Expression<Func<Color, bool>> filter)
+        {
+            using (CarsContext context = new CarsContext())
+            {
+                return context.Set<Color>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
+        {
+            using (CarsContext context = new CarsContext())
+            {
+                return filter == null ? context.Set<Color>().ToList() : context.Set<Color>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Color entity)
+        {
+            using (CarsContext context = new CarsContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
